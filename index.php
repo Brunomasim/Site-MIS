fed4a385915cba0f6b981d62ab95d46b81f1fbeb
Bruno Masim <simoes.j5@outlook.com>
2021-01-22 00:28:44 -0300

    criação de secção
    fiz uma alteração

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css"> 
+    <title>Museu da Imagem e do Som (MIS/AP)</title>
+</head>
+<body>
+   <img src="_imagens/logo.png" alt=""> 
+
+   <section></section>
+
+
+
+</body>
+</html>
